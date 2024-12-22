365caa590facf061394ce55df7d6d46901ab27ab
Kivashin Naidoo <kivashin47@gmail.com>
2024-12-22 20:33:16 +0200

    Basic components and hello

---
+++ react-project/src/Message.tsx
@@ -0,0 +1,10 @@
+//Recommended to use function based components
+//Basically you return a collection of html elements
+function Message() {
+    return (
+        <h1>Hello world</h1>
+    );
+}
+
+//Export it
+export default Message;
