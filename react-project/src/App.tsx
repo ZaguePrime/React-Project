d1a054e7b9fb4a86a037ce77dea3f30f07811cf8
Kivashin Naidoo <kivashin47@gmail.com>
2024-12-27 10:50:36 +0200

    Learned about properties and states

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,12 +1,22 @@
-import Message from './Message'
-import ListGroup from './components/ListGroup'
+import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup/>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
--- react-project/src/components/ListGroup.tsx
+++ react-project/src/components/ListGroup.tsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
 
-function ListGroup() {
-  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
-  
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
   //This is a hook that allows you to add state to a functional component
   const [selectedIndex, setSelectedIndex] = useState(-1);
-  
+
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No Items</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className={selectedIndex === index ? "list-group-item active" : "list-group-item"} key={index} onClick={() => {setSelectedIndex(index);}}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={index}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
             {item}
           </li>
         ))}
